fa187da7ee6412e18070a104e3b15fa4e4ed4094
NjokiM <njoki6m@gmail.com>
2026-04-19 17:32:44 +0300

    edit on the set_difference script

--- sql/working_with_multiple_tables_Part_2/set_difference.sql
+++ sql/working_with_multiple_tables_Part_2/set_difference.sql
@@ -1,12 +1,11 @@
 SELECT sr_sov_a3
 FROM   boundary_data_polygons_entities_geospatial_data
-ORDER BY iso_a3;
 EXCEPT 
 SELECT iso_a3
 FROM   boundary_data_countries_geospatial_data
 ORDER BY sr_sov_a3;
 
--- The NOT IN query can fail silently if NULLs exist in the subquery result thus the WHERE use
+-- The NOT IN query can fail silently if NULLs exist in the subquery result thus the WHERE..IS NOT NULLuse
 
 SELECT sr_sov_a3
 FROM boundary_data_polygons_entities_geospatial_data
